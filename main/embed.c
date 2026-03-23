68a2eaf316e0affa3ebf2a1f8dcb42180a26d631
Fritz Elfert <fritz@fritz-elfert.de>
2026-03-23 08:25:59 +0100

    Switch to DER-encoded certs/keys internally
    - Uses less memory

--- main/embed.c
+++ main/embed.c
@@ -9,20 +9,20 @@
  */
 #include "embed.h"
 
-extern uint8_t d_broker_crt_start[] asm("_binary_broker_crt_start");
-extern uint8_t d_broker_crt_end[]   asm("_binary_broker_crt_end");
-extern uint8_t d_client_crt_start[] asm("_binary_client_crt_start");
-extern uint8_t d_client_crt_end[]   asm("_binary_client_crt_end");
-extern uint8_t d_client_key_start[] asm("_binary_client_key_start");
-extern uint8_t d_client_key_end[]   asm("_binary_client_key_end");
-extern uint8_t d_ca_crt_start[]     asm("_binary_ca_crt_start");
-extern uint8_t d_ca_crt_end[]       asm("_binary_ca_crt_end");
+extern uint8_t d_broker_crt_der_start[] asm("_binary_broker_crt_der_start");
+extern uint8_t d_broker_crt_der_end[]   asm("_binary_broker_crt_der_end");
+extern uint8_t d_client_crt_der_start[] asm("_binary_client_crt_der_start");
+extern uint8_t d_client_crt_der_end[]   asm("_binary_client_crt_der_end");
+extern uint8_t d_client_key_der_start[] asm("_binary_client_key_der_start");
+extern uint8_t d_client_key_der_end[]   asm("_binary_client_key_der_end");
+extern uint8_t d_ca_crt_der_start[]     asm("_binary_ca_crt_der_start");
+extern uint8_t d_ca_crt_der_end[]       asm("_binary_ca_crt_der_end");
 
-uint8_t *broker_crt_start = d_broker_crt_start;
-uint8_t *broker_crt_end   = d_broker_crt_end;
-uint8_t *client_crt_start = d_client_crt_start;
-uint8_t *client_crt_end   = d_client_crt_end;
-uint8_t *client_key_start = d_client_key_start;
-uint8_t *client_key_end   = d_client_key_end;
-uint8_t *ca_crt_start     = d_ca_crt_start;
-uint8_t *ca_crt_end     = d_ca_crt_end;
+uint8_t *broker_crt_der_start = d_broker_crt_der_start;
+uint8_t *broker_crt_der_end   = d_broker_crt_der_end;
+uint8_t *client_crt_der_start = d_client_crt_der_start;
+uint8_t *client_crt_der_end   = d_client_crt_der_end;
+uint8_t *client_key_der_start = d_client_key_der_start;
+uint8_t *client_key_der_end   = d_client_key_der_end;
+uint8_t *ca_crt_der_start     = d_ca_crt_der_start;
+uint8_t *ca_crt_der_end     = d_ca_crt_der_end;
--- main/embed.h
+++ main/embed.h
@@ -2,16 +2,16 @@
 #include "stdint.h"
 #include "stddef.h"
 
-extern uint8_t *broker_crt_start;
-extern uint8_t *broker_crt_end;
-extern uint8_t *client_crt_start;
-extern uint8_t *client_crt_end;
-extern uint8_t *client_key_start;
-extern uint8_t *client_key_end;
-extern uint8_t *ca_crt_start;
-extern uint8_t *ca_crt_end;
+extern uint8_t *broker_crt_der_start;
+extern uint8_t *broker_crt_der_end;
+extern uint8_t *client_crt_der_start;
+extern uint8_t *client_crt_der_end;
+extern uint8_t *client_key_der_start;
+extern uint8_t *client_key_der_end;
+extern uint8_t *ca_crt_der_start;
+extern uint8_t *ca_crt_der_end;
 
-#define broker_crt_bytes (size_t)(broker_crt_end - broker_crt_start)
-#define client_crt_bytes (size_t)(client_crt_end - client_crt_start)
-#define client_key_bytes (size_t)(client_key_end - client_key_start)
-#define ca_crt_bytes     (size_t)(ca_crt_end     - ca_crt_start)
+#define broker_crt_der_bytes (size_t)(broker_crt_der_end - broker_crt_der_start)
+#define client_crt_der_bytes (size_t)(client_crt_der_end - client_crt_der_start)
+#define client_key_der_bytes (size_t)(client_key_der_end - client_key_der_start)
+#define ca_crt_der_bytes     (size_t)(ca_crt_der_end     - ca_crt_der_start)
